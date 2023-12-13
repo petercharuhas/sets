766d4d5c585277893a05e254b8ba48220c48350f
petercharuhas <139315928+petercharuhas@users.noreply.github.com>
2023-12-12 19:07:01 -0800

    Add files via upload

---
+++ sets.js
@@ -0,0 +1,28 @@
+/* Question 1 */
+// {1,2,3,4}
+
+/* Question 2 */
+// "ref"
+
+/* Question 3 */
+// 0: {[1, 2, 3] => true}
+// 1: {[1, 2, 3] => false}
+
+/* Question 3 */
+// const hasDuplicate = (arr) => arr.some((value, index) => arr.indexOf(value) !== index);
+
+/* Question 4 */
+/* const vowelCount = (str) => {
+  const vowels = "aeiou";
+  const vowelMap = new Map();
+
+  for (const char of str.toLowerCase()) {
+    if (vowels.includes(char)) {
+      vowelMap.set(char, (vowelMap.get(char) || 0) + 1);
+    }
+  }
+
+  return vowelMap;
+};
+*/
+
